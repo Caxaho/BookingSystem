7493c6031df50ae7de239c4f44b23e921ed61ca8
Caxaho <127947983+Caxaho@users.noreply.github.com>
2024-04-06 23:23:47 +0100

    Create VenueManager.java

---
+++ src/VenueManager.java
@@ -0,0 +1,6 @@
+public class VenueManager extends User{
+    public VenueManager(String name, String username, String emailAddress, String password) {
+        super(name, username, emailAddress, password);
+        this.setAccountType(AccountType.VENUE_MANAGER);
+    }
+}
