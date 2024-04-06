b449fdc233c99f59d8c604d84f59570de274c2ac
Caxaho <127947983+Caxaho@users.noreply.github.com>
2024-04-06 22:48:20 +0100

    Added user class

---
+++ src/User.java
@@ -0,0 +1,41 @@
+public class User {
+    private String name;
+    private String username;
+    private String emailAddress;
+    private String password;
+    private String mobileNo = "";
+    private int userID;
+    private static int userCount;
+    public enum AccountType {
+        AGENT,
+        CUSTOMER,
+        ADMIN,
+        VENUE_MANAGER
+    }
+    private AccountType accountType = AccountType.CUSTOMER;
+
+    public User(String name, String username, String emailAddress, String password) {
+        this.name = name;
+        this.username = username;
+        this.emailAddress = emailAddress;
+        this.password = password;
+    }
+
+    public String getName() { return name; }
+
+    public String getUsername() { return username; }
+
+    public int getID() { return userID; }
+
+    public String getEmail() { return emailAddress; }
+
+    public String getMobile() { return mobileNo; }
+
+    public void setMobile(String mobileNo) { this.mobileNo = mobileNo; }
+
+    public AccountType getAccountType() { return accountType; }
+
+    public void setAccountType(AccountType type) { accountType = type; }
+
+    public Boolean checkPW(String pass) { return password.equals(pass); }
+}
