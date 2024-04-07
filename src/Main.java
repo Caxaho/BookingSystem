11ff0170b7a8647586c4314f926846639e99cc8b
Caxaho <127947983+Caxaho@users.noreply.github.com>
2024-04-07 01:15:10 +0100

    Create initial state machine

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,136 @@
 public class Main {
+    public enum ProgramState {
+        START {
+            @Override
+            public ProgramState nextState(int choice) {
+                switch (choice) {
+                    case 0:
+                        return REQUEST_LOGIN;
+                    case 1:
+                        return REGISTRATION;
+                }
+                throw new IllegalArgumentException("choice out of bounds");
+            }
+
+            @Override
+            public ProgramState previousState() {
+                return START;
+            }
+        },
+        REGISTRATION {
+            @Override
+            public ProgramState nextState(int choice) {
+                return LOGGED_IN;
+            }
+
+            @Override
+            public ProgramState previousState() {
+                return START;
+            }
+        },
+        REQUEST_LOGIN {
+            @Override
+            public ProgramState nextState(int choice) {
+                return LOGGED_IN;
+            }
+
+            @Override
+            public ProgramState previousState() {
+                return START;
+            }
+        },
+        LOGGED_IN {
+            @Override
+            public ProgramState nextState(int choice) {
+                switch (choice) {
+                    case 0:
+                        return SELECT_SHOW;
+                    case 1:
+                        return CANCEL_SHOW;
+                }
+                throw new IllegalArgumentException("choice out of bounds");
+            }
+
+            @Override
+            public ProgramState previousState() {
+                return START;
+            }
+        },
+        SELECT_SHOW {
+            @Override
+            public ProgramState nextState(int choice) {
+                switch (choice) {
+                    case 0:
+                        return AUTOMATIC_SELECTION;
+                    case 1:
+                        return INTERACTIVE_SELECTION;
+                }
+                throw new IllegalArgumentException("choice out of bounds");
+            }
+
+            @Override
+            public ProgramState previousState() {
+                return LOGGED_IN;
+            }
+        },
+        AUTOMATIC_SELECTION {
+            @Override
+            public ProgramState nextState(int choice) {
+                switch (choice) {
+                    case 0:
+                        return PAYMENT;
+                    case 1:
+                        return INTERACTIVE_SELECTION;
+                }
+                throw new IllegalArgumentException("choice out of bounds");
+            }
+
+            @Override
+            public ProgramState previousState() {
+                return SELECT_SHOW;
+            }
+        },
+        INTERACTIVE_SELECTION {
+            @Override
+            public ProgramState nextState(int choice) {
+                return PAYMENT;
+            }
+
+            @Override
+            public ProgramState previousState() {
+                return SELECT_SHOW;
+            }
+        },
+        PAYMENT {
+            @Override
+            public ProgramState nextState(int choice) {
+                return LOGGED_IN;
+            }
+
+            @Override
+            public ProgramState previousState() {
+                return SELECT_SHOW;
+            }
+        },
+        CANCEL_SHOW {
+            @Override
+            public ProgramState nextState(int choice) {
+                return LOGGED_IN;
+            }
+
+            @Override
+            public ProgramState previousState() {
+                return LOGGED_IN;
+            }
+        };
+        public abstract ProgramState nextState(int choice);
+        public abstract ProgramState previousState();
+    }
+
+    static ProgramState state = ProgramState.START;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(state);
+        state = state.nextState(0);
+        System.out.println(state);
     }
 }
